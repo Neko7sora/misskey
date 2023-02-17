693f7b048066da03c49d8a9e63f348e4560feb03
Neko7sora <75793267+Neko7sora@users.noreply.github.com>
2023-02-18 01:48:10 +0900

    aaa

--- scripts/build-post.js
+++ scripts/build-post.js
@@ -3,13 +3,13 @@ const locales = require('../locales');
 const meta = require('../package.json');
 const v = { '_version_': meta.version };
 
-// copy:frontend:locales
+// frontend:locales
 fs.mkdirSync('./../built/_frontend_dist_/locales', { recursive: true });
 for (const [lang, locale] of Object.entries(locales)) {
 	fs.writeFileSync(`./../built/_frontend_dist_/locales/${lang}.${meta.version}.json`, JSON.stringify({ ...locale, ...v }), 'utf-8');
 }
 
-// build:backend:script
+// backend:script
 fs.rmSync(__dirname + '/../packages/backend/built/server/web/boot.js', { recursive: true, force: true });
 const content = fs.readFileSync(__dirname + '/../packages/backend/src/server/web/boot.js', { encoding: 'utf8' });
 const lines = content.replace('LANGS', JSON.stringify(Object.keys(locales))).split('\n');
